5c650ae94feb2e7378da2aa782254a1a0be82aa9
Gagan Nagar <gaganngr319@gmail.com>
2026-01-10 16:42:39 +0530

    update

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -3,13 +3,28 @@ import './App.css'
 
 function App() {
 
-  const [name, setname] = useState(0)
-  const [note, setnote] = useState(0)
+  const [Title, setTitle] = useState('')
+  const [Note, setNote] = useState('')
+  const [allNotes, setallNotes] = useState([])
 
   const formhandler = (e) => {
     e.preventDefault();    // form submit hone par page reload nhi hoga
-    console.log("okokokoko")
+    if(Title==='' || Note===''){
+       return alert('Write Your Note corretly')      //agar title or notes dono chaiye to return kardo nhi direct alert
+    }
+   const task = [...allNotes]
+   task.push({title:Title,note:Note});
+   setallNotes(task);
 
+   setTitle('')
+   setNote('')
+  }
+  
+  const dltNote=(idx)=>{
+    const dltTask=[...allNotes]
+
+    dltTask.splice(idx,1);
+    setallNotes(dltTask)
   }
 
   return (
@@ -20,16 +35,16 @@ function App() {
 
         <input type="text"
           placeholder='Enter Notes Heading'
-          value={name}
+          value={Title}
           onChange={(e) => {
-            setname(e.target.value)
+            setTitle(e.target.value)
           }} className='px-5 w-full font-medium py-2 border-2 outline-none rounded'
         />
 
         <textarea
           placeholder='write details'
-          value={note}
-          onChange={(e) => setnote(e.target.value)}
+          value={Note}
+          onChange={(e) => setNote(e.target.value)}
           className='px-5 w-full font-medium h-32 py-2 border-2 outline-none resize-none'
         >
         </textarea>
@@ -37,10 +52,18 @@ function App() {
       </form>
       <div className='lg:w-1/2 border-1-2 p-10'>
         <h1 className='text-3xl font-bold'>Your Notes</h1>
-        <div className='flex flex-wrap gap-5 mt-5 h-full overflow-auto' >
-          <div className='h-52 w-40 rounded-2xl bg-white'></div>
-          <div className='h-52 w-40 rounded-2xl bg-white'></div>
-          <div className='h-52 w-40 rounded-2xl bg-white'></div>
+        <div className='flex flex-wrap gap-5 mt-5 h-full overflow-auto'>
+          {allNotes.map((elem,idx)=>{
+            return <div key={idx} className=" flex justify-between flex-col items-start relative h-52 w-40 bg-cover rounded-xl text-black pt-9 pb-4 px-4 bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]">
+              <div>
+                <h3 className='leading-tight text-lg font-bold'>{elem.title}</h3>
+                <p className='mt-2 leading-tight text-xs font-semibold text-gray-600'>{elem.note}</p>
+              </div>
+              <button onClick={() => {
+                dltNote(idx)
+              }} className='w-full cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white'>Delete</button>
+            </div>
+          })}
         </div>
       </div>
     </div>
